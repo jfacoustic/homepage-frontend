1a93257dfefa26441c5bc3ec29863464367ae24d
Joshua F. Mathews <JoshFeltonM@gmail.com>
2020-06-13 18:03:18 -0500

    Introduction

--- src/homepage/home.cljs
+++ src/homepage/home.cljs
@@ -1,9 +1,14 @@
 (ns homepage.home)
 
 (defn home []
-    [:section {:class "hero is-fullheight"}
-    [:div {:class "hero-body"}
-        [:p (str 
-            "Hi, I'm Josh Mathews. "
-            "I'm a musician, software developer, and advocate of the Oxford Comma. "
-            "I'm currently a consultant for IBM in Baton Rouge, Louisiana. ")]]])
+  [:section {:class "hero is-fullheight"}
+   [:div {:class "hero-body"}
+    [:div {:class "container introduction"}
+     [:h1 {:class "title has-text-light"} "Introduction"]
+     [:p {:class "has-text-light"} (str
+                                    "Hi, I'm Josh Mathews. "
+                                    "I'm a musician, software developer, and advocate of the Oxford Comma. "
+                                    "I'm currently a consultant for IBM in Baton Rouge, Louisiana, building high-performance enterprise applications. "
+                                    "This site is currently in progress.  I'm rewriting it from the ground up with full-stack Clojure. ")]
+     [:p {:class "has-text-light has-text-right side-note"} "Some friends and I took this picture!  We were sponsored by the Colorado Space Grant Consortium to launch a ballon-sat. "
+      [:a {:href "http://thecrite.com/coloradomesau/ground-control-major-tom/"} "More Info"]]]]])
--- src/homepage/navbar.cljs
+++ src/homepage/navbar.cljs
@@ -1,21 +1,20 @@
-(ns homepage.navbar )
+(ns homepage.navbar)
 
 (defn navbar []
-    [:nav {:class "navbar" :role "navigation" :aria-label "main navigation"} 
-        [:div {:class "navbar-brand"}
-            [:a {:class "navbar-item" :href "/"}
-                "Josh Felton Mathews"]
-            [:a {:role "button"
-                 :class "navbar-burger burger"
-                 :aria-label "menu"
-                 :aria-expanded "false"
-                 :data-target "navbarBasicExample"}
-                 [:span {:aria-hidden "true"}]
-                 [:span {:aria-hidden "true"}]
-                 [:span {:aria-hidden "true"}]
-                 ]]
-        [:div {:id "navbarBasicExample" :class "navbar-menu"}
-            [:div {:class "navbar-start"}
-                [:a {:class "navbar-item" :href "http://www.joshfeltonmathews.com"} "Music"]
-                [:a {:class "navbar-item" :href "http://www.github.com/jfacoustic"} "Github"]
-                [:a {:class "navbar-item" :href "http://resume.joshfeltonmathews.com"} "Resume"]]]])
+  [:nav {:class "navbar" :role "navigation" :aria-label "main navigation"}
+   [:div {:class "navbar-brand"}
+    [:a {:class "navbar-item navbar-title" :href "/"}
+     "Josh Felton Mathews"]
+    [:a {:role "button"
+         :class "navbar-burger burger"
+         :aria-label "menu"
+         :aria-expanded "false"
+         :data-target "navbarBasicExample"}
+     [:span {:aria-hidden "true"}]
+     [:span {:aria-hidden "true"}]
+     [:span {:aria-hidden "true"}]]]
+   [:div {:id "navbarBasicExample" :class "navbar-menu"}
+    [:div {:class "navbar-start"}
+     [:a {:class "navbar-item" :href "http://www.joshfeltonmathews.com"} "Music"]
+     [:a {:class "navbar-item" :href "http://www.github.com/jfacoustic"} "Github"]
+     [:a {:class "navbar-item" :href "http://resume.joshfeltonmathews.com"} "Resume"]]]])
