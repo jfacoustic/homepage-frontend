5d9ca12d6010dc3e39752524702fbf4b1894d2be
Joshua F. Mathews <JoshFeltonM@gmail.com>
2020-06-13 22:30:40 -0500

    navbar refactoring

--- src/homepage/navbar.cljs
+++ src/homepage/navbar.cljs
@@ -1,20 +1,25 @@
 (ns homepage.navbar)
 
+(defn burger []
+  [:a {:role "button"
+       :class "navbar-burger burger"
+       :aria-label "menu"
+       :aria-expanded "false"
+       :data-target "navbarBasicExample"}
+   [:span {:aria-hidden "true"}]
+   [:span {:aria-hidden "true"}]
+   [:span {:aria-hidden "true"}]])
+
+(defn navbar-menu []
+  [:div {:id "navbarBasicExample" :class "navbar-menu"}
+   [:div {:class "navbar-start"}
+    [:a {:class "navbar-item" :href "http://www.joshfeltonmathews.com"} "Music"]
+    [:a {:class "navbar-item" :href "http://www.github.com/jfacoustic"} "Github"]
+    [:a {:class "navbar-item" :href "http://resume.joshfeltonmathews.com"} "Resume"]]])
 (defn navbar []
   [:nav {:class "navbar" :role "navigation" :aria-label "main navigation"}
    [:div {:class "navbar-brand"}
     [:a {:class "navbar-item navbar-title" :href "/"}
      "Josh Felton Mathews"]
-    [:a {:role "button"
-         :class "navbar-burger burger"
-         :aria-label "menu"
-         :aria-expanded "false"
-         :data-target "navbarBasicExample"}
-     [:span {:aria-hidden "true"}]
-     [:span {:aria-hidden "true"}]
-     [:span {:aria-hidden "true"}]]]
-   [:div {:id "navbarBasicExample" :class "navbar-menu"}
-    [:div {:class "navbar-start"}
-     [:a {:class "navbar-item" :href "http://www.joshfeltonmathews.com"} "Music"]
-     [:a {:class "navbar-item" :href "http://www.github.com/jfacoustic"} "Github"]
-     [:a {:class "navbar-item" :href "http://resume.joshfeltonmathews.com"} "Resume"]]]])
+    [burger]]
+   [navbar-menu]])
